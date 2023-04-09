265aaf3165279148357fa10d7eb2c782cfe77359
Myst33d <notmyst33d@gmail.com>
2023-04-09 22:41:39 +0600

    ch1: Update levels

--- ch1/level/bedroom.gd
+++ ch1/level/bedroom.gd
@@ -4,6 +4,9 @@ onready var player_node = get_node(player)
 onready var dialog_ui = get_node(level_camera).get_node("UI/Control/DialogUI")
 
 func _ready():
+	if Global.variables.get("bedroom_dialog_finished"):
+		return
+
 	dialog_ui.connect("dialog_finished", self, "dialog_finished")
 	dialog_ui.connect("control_changed", self, "control_changed")
 	dialog_ui.load_ddf("res://ch1/dialog/asriel.tres")
@@ -19,3 +22,4 @@ func dialog_finished():
 	dialog_ui.disconnect("dialog_finished", self, "dialog_finished")
 	dialog_ui.disconnect("control_changed", self, "control_changed")
 	player_node.enable_movement()
+	Global.variables["bedroom_dialog_finished"] = true
