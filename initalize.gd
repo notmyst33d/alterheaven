1b7ce21a9ed9b414e999b93397a1ded0694ca0f9
Myst33d <notmyst33d@gmail.com>
2023-06-29 23:15:27 +0600

    Fix font rendering once and for all

--- initalize.gd
+++ initalize.gd
@@ -1,4 +1,14 @@
 extends Node
 
+var tvres = Vector2(1280, 960)
+var wideres = Vector2(1696, 954)
+
 func _ready():
+    #OS.window_size = wideres
+    #get_tree().set_screen_stretch(SceneTree.STRETCH_MODE_VIEWPORT, SceneTree.STRETCH_ASPECT_KEEP, wideres)
+
+    # Yes fuck you filter my fucking viewport
+    get_node("/root").get_texture().flags = Texture.FLAG_FILTER
+
+    # For testing
     get_tree().change_scene("res://ch1/level/bedroom.tscn")
